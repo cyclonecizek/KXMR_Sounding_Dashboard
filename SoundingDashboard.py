0e88b9e3cfae27ae9f866daa0f776a913d57cde6
cyclonecizek <153048634+cyclonecizek@users.noreply.github.com>
2024-02-29 16:16:56 -0500

    Add files via upload

--- SoundingDashboard.py
+++ SoundingDashboard.py
@@ -81,6 +81,13 @@ point = base.mark_circle(size=50).encode(
 
 reg_line = point.transform_regression(item1, item2).mark_line()
 
+text = alt.Chart({'values':[{}]}).mark_text(
+    align="left", baseline="top"
+).encode(
+    x=alt.value(5),  # pixels from left
+    y=alt.value(5),  # pixels from top
+    text=alt.value(f"r: {corl:.3f}"),
+)
 
 # Layout (Content)
 left_column, right_column = st.columns(2)
@@ -91,4 +98,4 @@ left_column.altair_chart(bar, theme = None, use_container_width=True)
 
 right_column.markdown(
     '**Scatter Plot of _' + item1 + '_ and _' + item2 + '_**')
-right_column.altair_chart(point+reg_line, theme = None, use_container_width=True)
+right_column.altair_chart(point+text+reg_line, theme = None, use_container_width=True)
